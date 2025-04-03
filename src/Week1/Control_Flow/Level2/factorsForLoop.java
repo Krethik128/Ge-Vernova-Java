912ed7e84aa6d265fd68269a65b425f714c4cb34
Krethik128 <krethik3114@gmail.com>
2025-04-03 15:39:11 +0530

    Completed Control Flow level 2

---
+++ src/Week1/Control_Flow/Level2/factorsForLoop.java
@@ -0,0 +1,25 @@
+package src.Week1.Control_Flow.Level2;
+
+import java.util.Scanner;
+
+public class factorsForLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt();
+
+        if (number > 0) {
+            System.out.println("The factors of " + number + " are:");
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    System.out.println(i);
+                }
+            }
+        } else {
+            System.out.println("Please enter a positive integer.");
+        }
+
+    }
+}
+
---
+++ src/Week1/Control_Flow/Level2/greatestFactorForLoop.java
@@ -0,0 +1,25 @@
+package src.Week1.Control_Flow.Level2;
+
+import java.util.Scanner;
+
+public class greatestFactorForLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt();
+        int greatestFactor = 1;
+
+        if (number > 1) {
+            for (int i = number - 1; i > 0; i--) {
+                if (number % i == 0) {
+                    greatestFactor = i;
+                    break;
+                }
+            }
+            System.out.println("The greatest factor of " + number + " besides itself is: " + greatestFactor);
+        } else {
+            System.out.println("Please enter a positive integer greater than 1.");
+        }
+    }
+}
