14d54b3f9ae334da50f9b6463bcacf0e79ad575a
Krethik128 <krethik3114@gmail.com>
2025-04-06 19:08:52 +0530

    String level1 small update

--- src/Week1/Strings/Touppercase.java
+++ src/Week1/Strings/Touppercase.java
@@ -15,6 +15,17 @@ public class Touppercase {
             }
             return result;
         }
+        public static boolean compareStrings(String s1, String s2) {
+            if (s1.length() != s2.length()) {
+                return false;
+            }
+            for (int i = 0; i < s1.length(); i++) {
+                if (s1.charAt(i) != s2.charAt(i)) {
+                    return false;
+                }
+            }
+            return true;
+        }
         public static void main(String[] args){
             Scanner sc=new Scanner(System.in);
             String str=sc.nextLine();
