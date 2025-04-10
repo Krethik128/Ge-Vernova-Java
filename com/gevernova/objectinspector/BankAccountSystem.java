7d3aa7f6940d0b1f35f57452b203a118a51da962
Krethik128 <krethik3114@gmail.com>
2025-04-10 20:33:59 +0530

    Practising Static,final,instance and this keyword

---
+++ com/gevernova/objectinspector/BankAccountSystem.java
@@ -0,0 +1,41 @@
+package com.gevernova.objectinspector;
+class BankAccount{
+    public static String bankName="ABC Bank";;
+    public String accountHolderName;
+    private final int accountNumber;
+    private static int totalAccounts = 0;
+    BankAccount(String accountHolderName,int accountNumber){
+        this.accountHolderName=accountHolderName;
+        this.accountNumber=accountNumber;
+        totalAccounts++; // update account count when new object created
+
+    }
+
+    public static void getTotalAccounts(){
+        System.out.println("Total Accounts: " + totalAccounts);
+    }
+    // Method to display account details
+    public void displayAccountDetails() {
+        System.out.println("Bank Name: " + bankName);
+        System.out.println("Account Holder: " + accountHolderName);
+        System.out.println("Account Number: " + accountNumber);
+    }
+}
+public class BankAccountSystem {
+    public static void main(String[] args) {
+        BankAccount acc1 = new BankAccount("Alice", 1001);
+        BankAccount acc2 = new BankAccount("Bob", 1002);
+
+        // instanceof check before displaying details
+        if (acc1 instanceof BankAccount) {
+            acc1.displayAccountDetails();
+        }
+
+        if (acc2 instanceof BankAccount) {
+            acc2.displayAccountDetails();
+        }
+
+        // Call static method
+        BankAccount.getTotalAccounts();
+    }
+}
