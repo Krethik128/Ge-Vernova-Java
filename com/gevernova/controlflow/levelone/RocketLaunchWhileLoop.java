aaabe73c3268ba4d4d4e5d5460774381e45522f0
Krethik128 <krethik3114@gmail.com>
2025-04-11 16:48:02 +0530

    Corrected class name

--- com/gevernova/controlflow/levelone/RocketLaunchWhileLoop.java
+++ com/gevernova/controlflow/levelone/RocketLaunchWhileLoop.java
@@ -2,7 +2,7 @@ package com.gevernova.controlflow.levelone;
 
 import java.util.Scanner;
 
-public class Rocket_Launch_WhileLopp {
+public class RocketLaunchWhileLoop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the countdown starting number: ");
