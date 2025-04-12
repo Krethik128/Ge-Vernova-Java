1ba73eaafe83e309f33acb28c6102aaf0b586d5e
Krethik128 <krethik3114@gmail.com>
2025-04-12 15:29:17 +0530

    done type casting

--- com/gevernova/programmingelements/AreaOfTriangle.java
+++ com/gevernova/programmingelements/AreaOfTriangle.java
@@ -9,8 +9,7 @@ public class AreaOfTriangle {
         double height = sc.nextDouble();
         
         double areaCm = 0.5 * base * height;
-        double areaIn = areaCm / (2.54 * 2.54);
-        
+        int areaIn =(int)(areaCm / (2.54 * 2.54));//Type casting
         System.out.println("The area of the triangle is " + areaCm + " square cm and " + areaIn + " square inches.");
     }
 }
